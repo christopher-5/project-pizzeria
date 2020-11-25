0ca9326aa54a3be2a4e4744b745f3697f646dca7
Krzysztof Wanat <krzysiekwanat@gmail.com>
2020-11-25 18:14:31 +0100

    rangeSlider to fix

--- src/js/components/AmountWidget.js
+++ src/js/components/AmountWidget.js
@@ -1,70 +1,54 @@
 import {settings, select} from '../settings.js';
+import BaseWidget from './BaseWidget.js';
 
-export class AmountWidget {
+export class AmountWidget extends BaseWidget{
   constructor(element){
+    super(element, settings.amountWidget.defaultValue);
     const thisWidget = this;
       
-    thisWidget.getElements(element);
-    thisWidget.value = settings.amountWidget.defaultValue;
-    thisWidget.setValue(thisWidget.input.value);
-    // console.log(thisWidget.input.value);
+    thisWidget.getElements(element);   
+  
     thisWidget.initActions();
-    // console.log('AmoutnWidget:', thisWidget);
-    // console.log('constructor arguments:', element);
   }
-  annouce(){
+  
+  isValid(value){
+    return !isNaN(value) 
+      && value >= settings.amountWidget.defaultMin 
+      && value <= settings.amountWidget.defaultMax;
+  }
+
+  renderValue(){
     const thisWidget = this;
 
-    const event = new CustomEvent('updated', {
-      bubbles: true
-    });
-      // console.log(event);
-    thisWidget.element.dispatchEvent(event);
-    // console.log(event);
+    thisWidget.dom.input.value = thisWidget.value;
   }
-  setValue(value){
-    const thisWidget = this;
-     
-    const newValue = parseInt(value);
-    // console.log(thisWidget);
 
-    if (value !== thisWidget.value && value >= settings.amountWidget.defaultMin && value <= settings.amountWidget.defaultMax) {
-      thisWidget.value = newValue;
-      thisWidget.annouce();
-    }
-    thisWidget.input.value = thisWidget.value;
-  }
   initActions(){
     const thisWidget = this;
      
-    thisWidget.input.addEventListener('change', thisWidget.setValue(thisWidget.value));
-    thisWidget.linkDecrease.addEventListener('click', thisWidget.linkDecreaseHandler.bind(thisWidget));
-    // console.log(thisWidget.linkDecreaseHandler);
-    thisWidget.linkIncrease.addEventListener('click', thisWidget.linkIncreaseHandler.bind(thisWidget));
+    thisWidget.dom.input.addEventListener('change', thisWidget.setValue(thisWidget.value));
+    thisWidget.dom.linkDecrease.addEventListener('click', thisWidget.linkDecreaseHandler.bind(thisWidget));
+    thisWidget.dom.linkIncrease.addEventListener('click', thisWidget.linkIncreaseHandler.bind(thisWidget));
   }
   linkDecreaseHandler(event){
     const thisWidget = this;
     event.preventDefault();
     const value = thisWidget.value - 1; 
-    // console.log(value);
     thisWidget.setValue(value);
   }
   linkIncreaseHandler(event){
     event.preventDefault();
     const thisWidget = this; 
-    const value = thisWidget.value + 1; 
-    // console.log(value);
+    const value = thisWidget.value + 1;
     thisWidget.setValue(value);
   }
    
-  getElements(element){
+  getElements(){
     const thisWidget = this;
-      
-    thisWidget.element = element;
-    console.log(element);
-    thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
-    thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
-    thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.amount.input);
+    thisWidget.dom.linkDecrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkDecrease);
+    thisWidget.dom.linkIncrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkIncrease);
   }
 }
 
---
+++ src/js/components/DatePicker.js
@@ -0,0 +1,54 @@
+import BaseWidget from './BaseWidget.js';
+import {utils} from '../utils.js';
+import {select, settings} from '../settings.js';
+
+
+export class DatePicker extends BaseWidget{
+  constructor(wrapper){
+    super(wrapper, utils.dateToStr(new Date()));
+    const thisWidget = this;
+
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.datePicker.input);
+    thisWidget.initPlugin();
+  }
+
+  initPlugin(){
+    const thisWidget = this;
+    thisWidget.minDate = new Date(thisWidget.value);
+    thisWidget.maxDate = utils.addDays(thisWidget.minDate + settings.datePicker.maxDaysInFuture);
+
+    // eslint-disable-next-line no-undef
+    flatpickr(thisWidget.dom.input, {
+      defaultDate: thisWidget.minDate,
+      minDate: thisWidget.minDate,
+      maxDate: thisWidget.maxDate,    
+      'disable': [
+        function(date) {
+          return (date.getDay() === 1);
+        }
+      ],
+      'locale': {
+        'firstDayOfWeek': 1
+      },
+      // eslint-disable-next-line no-unused-vars
+      onChange: function(selectedDates, dateStr, instance) {
+        thisWidget.value = dateStr;
+        console.log('thisWidget.value:', thisWidget.value);
+      }
+    });
+
+  }
+
+  parseValue(value){
+    return value;
+  }
+
+  isValid(){
+    return true;
+  }
+
+  renderValue(){
+
+  }
+}
+
